7cb76930750972cf7f1a782cb994e1ea697e942d
mikelubs <michaelubembe@gmail.com>
2017-04-11 13:32:42 +0300

    add and style menu

--- js/sripts.js
+++ js/sripts.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
        $(".clickable1").click(function() {
          $(".menu1").toggle();
          $(".menu2").toggle();
+         $("ul").show();
        });
 
        $(".clickable2").click(function() {
          $(".menu2").toggle();
          $(".menu1").toggle();
+         $("ul").hide();
        });
 
      });

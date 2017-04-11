9961f771b37d5dad871c1e0eca809c589cb11ec6
mikelubs <michaelubembe@gmail.com>
2017-04-11 23:35:21 +0300

    add hover effects on hamburger menu

--- js/sripts.js
+++ js/sripts.js
@@ -3,14 +3,14 @@ $(document).ready(function() {
          $(".menu1").toggle();
          $(".menu2").toggle();
          $(".topp").slideDown();
-         $(".first1").slideUp();
+         $(".largest").slideUp();
        });
 
        $(".clickable2").click(function() {
          $(".menu2").toggle();
          $(".menu1").toggle();
          $(".topp").slideUp();
-         $(".first1").slideDown();
+         $(".largest").slideDown();
        });
 
      });

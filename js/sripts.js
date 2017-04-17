bddc06c44aca1bb6d0177205644d4060388eca68
mikelubs <michaelubembe@gmail.com>
2017-04-18 01:48:19 +0300

    add mailchimp

--- js/sripts.js
+++ js/sripts.js
@@ -13,4 +13,36 @@ $(document).ready(function() {
          $(".largest").fadeIn();
        });
 
+       $('.mc-form-example').submit(function(e){
+
+       //prevent the form from submitting via the browser redirect
+       e.preventDefault();
+
+       //grab attributes and values out of the form
+       var data = {email: $('#mc-email').val()};
+       var endpoint = $(this).attr('action');
+
+       //make the ajax request
+       $.ajax({
+         method: 'POST',
+         dataType: "json",
+         url: endpoint,
+         data: data
+       }).success(function(data){
+         if(data.id){
+           //successful adds will have an id attribute on the object
+           alert('thanks you for subscribing');
+         } else if (data.title == 'Member Exists') {
+           //MC wil send back an error object with "Member Exists" as the title
+           alert('We love you, you have already subscribed');
+         } else {
+           //something went wrong with the API call
+           alert('Oh no, there has been a problem');
+         }
+       }).error(function(){
+         //the AJAX function returned a non-200, probably a server problem
+         alert('Oh no, there has been a problem');
+       });
+     });
+
      });

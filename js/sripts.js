d70fc70a1ac4ceee739be1482f96945c72097594
mikelubs <michaelubembe@gmail.com>
2017-04-19 01:29:48 +0300

    add mail chimp

--- js/sripts.js
+++ js/sripts.js
@@ -13,35 +13,4 @@ $(document).ready(function() {
          $(".largest").fadeIn();
        });
 
-       $('.subscribe-form').submit(function(e){
-       	//prevent the form from submitting via the browser redirect
-       	e.preventDefault();
-
-       	//grab attributes and values out of the form
-       	var data = {email: $('#subscribe-email').val()};
-       	var endpoint = $(this).attr('action');
-
-       	//make the ajax request
-       	$.ajax({
-       		method: 'POST',
-       		dataType: "json",
-       		url: endpoint,
-       		data: data
-       	}).done(function(data){
-       		if(data.id){
-       			//successful adds will have an id attribute on the object
-       			alert('thanks for signing up');
-       		} else if (data.title == 'Member Exists') {
-       			//MC wil send back an error object with "Member Exists" as the title
-       			alert('thanks, but you are alredy signed up');
-       		} else {
-       			//something went wrong with the API call
-       			alert('oh no, there has been a problem');
-       		}
-       	}).fail(function(){
-       		//the AJAX function returned a non-200, probably a server problem
-       		alert('oh no, there has been a problem');
-       	});
-       });
-
      });
